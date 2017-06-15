d04b740060f0b9ce464a8207e7d54e8ffc64a3a2
terrence386 <814324580@qq.com>
2017-06-15 09:28:23 +0800

    first commit
    Build project skeleton
    搭建项目骨架

---
+++ src/app/Header/Header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'; //引入核心模块
+
+//添加元信息
+@Component({
+	selector: 'snail-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
+})
+
+//导出组件
+export class HeaderComponent {
+	
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+//在这里引入自己定义的模块组件
+import { HeaderComponent } from './Header/Header.component'
+
+@NgModule({
+	//声明组件
+  declarations: [
+    AppComponent,
+	HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
